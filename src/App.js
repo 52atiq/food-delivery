666a7cc51f0202efa81ca853e274578cee2262d5
unknown <atiqit52@gmail.com>
2022-08-22 13:32:13 +0600

    header set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Header } from './components';
 
 const App = () => {
     return (
-        <div className='text-red-500'>
-            App
+        <div className='w-screen h-auto flex flex-col'>
+            <Header />
         </div>
     );
 };
